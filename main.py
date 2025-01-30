5c906bb690a43ec3fa3bdd93ae257cde3b414a17
kad568 <kadmiel.m5@gmail.com>
2025-01-30 21:45:48 +0000

    added main script for ML methods

---
+++ main.py
@@ -0,0 +1,16 @@
+import sklearn
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+DB_FILE_NAME = "btc_onchain.db"
+
+engine = create_engine(f'sqlite:///{DB_FILE_NAME}')
+
+# example pull from db
+table_name = "STH_percent_supply_in_profit_7D_MA"
+query = f"SELECT * FROM {table_name}"
+df = pd.read_sql(query, engine)
+
+
+print(df)
